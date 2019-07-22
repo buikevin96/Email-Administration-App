d3cabf4ce5494200443ee7e7edbbafd815797ea7
Kevin Bui <buikevin96@gmail.com>
2019-07-22 16:56:00 -0400

    Built constructor and setDepartment method

---
+++ Email Administration App/src/emailapp/Email.java
@@ -0,0 +1,49 @@
+package emailapp;
+
+import java.util.Scanner;
+
+;public class Email {
+
+	private String firstName;
+	private String lastName;
+	private String password;
+	private String department;
+	private int mailboxCapacity;
+	private String alternateEmail;
+	
+	// Constructor to receive the first name and last name
+	public Email(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		System.out.println("Email Created: " + this.firstName + " " + this.lastName);
+		
+		// Call a method asking for the department - return the department
+		this.department = setDepartment();
+		System.out.println("Department: " + this.department);
+	}
+	
+	// Ask for department
+	private String setDepartment() {
+		System.out.print("Department Codes:\n1 for Sales\n2 for Development\n3 for Accounting\n0 for Nothing\nEnter department code: ");
+		Scanner in = new Scanner (System.in);
+		int depChoice = in.nextInt();
+		
+		if (depChoice == 1) {
+			return "sales";
+		} else if (depChoice == 2){
+			return "dev";
+		} else if (depChoice == 3){
+			return "acct";
+		} else {
+			return "";
+		}
+	}
+	
+	// Generate a random password
+	
+	// Set the mailbox capacity
+	
+	// Set the alternate email
+	
+	// Change the password
+}
